78c532217a52847b8bf0afe6205abebf9f9c33e2
Valki11 <102394314+Valki11@users.noreply.github.com>
2024-03-20 22:59:14 -0600

    Add files via upload

---
+++ PUNTEROS2.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+
+using namespace std;
+
+main(){
+	int edad=24,*p_edad;
+	p_edad= &edad;
+	
+	cout<<"var Edad"<<edad<<endl;
+	cout<<"puntero edad"<<*p_edad<<endl;
+	edad=40;
+	
+	cout<<"var Edad"<<edad<<endl;
+	cout<<"puntero edad"<<*p_edad<<endl;
+	
+cout<<"variable otra "<<edad<<endl; 
+int edad2=100;
+p_edad=&edad},
+cout<<"variable otra "<<edad<<endl; 
+cout<<"variable otra "<<edad<<endl; 	
+	system_("pause");
+	
---
+++ Punterosssssss.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+
+using namespace std;
+
+main(){
+int fil=0,col=0, **pm_notas;
+cout<<"Ingrese la cantidad estudiantes : ";	
+cin>>fil;
+cout<<"Ingrese la cantidad notas por estudiantes : ";	
+cin>>col;
+
+pm_notas=new int *[fil];
+for(int=0; i<fil;i++){
+	for(int ii};)
+	pm_notas[i]=new int[col];
+}
+for(int i=0; i<fil;i++){
+	
+}
+
+	
+	system("PAUSE");
+
+}
